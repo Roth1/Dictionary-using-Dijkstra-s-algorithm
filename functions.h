7afaf930162758a2403ef4345d9f414eec7ae062
Roth1 <roth.man@web.de>
2015-04-20 15:29:20 +0200

    All files assembled

---
+++ functions.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTIONS_H_
+#define FUNCTIONS_H_
+
+#include <stdio.h>
+#include <limits.h>
+
+//creer_table_de_hachage();
+//Prototypes here
+
+
+#endif
---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "functions.h"
+
+int main() 
+{
+  // Enter code here
+  return 0;
+}
