b784fe77aae0b7ccec36d62c22c4d4acf416b84a
Roth1 <roth.man@web.de>
2015-05-04 23:34:26 +0200

    Lists improved.

--- liste.h
+++ liste.h
@@ -14,8 +14,9 @@ typedef struct cellule* Liste;
 Liste creer_liste(void);
 int est_vide(Liste L);
 Liste ajout_tete(unsigned char *e, Liste L);
-Liste rech_chaine(char *chaine, Liste L);
-Liste rech(unsigned char *e, Liste L);
+//Liste rech_chaine(char *chaine, Liste L);
+//Liste rech(unsigned char *e, Liste L);
+int recherche_liste(unsigned char *e, Liste L);
 int taille_liste(Liste L);
 
 #endif
